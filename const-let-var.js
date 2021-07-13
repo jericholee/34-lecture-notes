3a4d1585a615cafae28034662ab8f519c3cb97d3
Jericho Ortiz <jortiz180@cnm.edu>
2021-07-13 16:36:10 -0600

    Notes updated

---
+++ const-let-var.js
@@ -0,0 +1,10 @@
+// at first use of a variable, you must declare it with const, let or var
+
+// use const when you need to change a val
+
+// use let when you need to change value
+let age = 27
+age = age +1
+
+// use var when you want your code to break
+var planet = 'earth'
