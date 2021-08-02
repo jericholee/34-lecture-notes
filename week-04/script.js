d320b5dd919c1bbcc8cfe6dd87bb6e88014ad50d
Jericho Ortiz <jortiz180@cnm.edu>
2021-08-02 09:04:47 -0600

    added Snap Challenge Javascript forEach

---
+++ week-04/script.js
@@ -0,0 +1,18 @@
+//Snap Challenge
+//Using the foreach loop display this array of text to the DOM.
+//
+// ['Bob', 'Jim', 'Matt', 'Mary', 'Suzy', 'Sarah']
+//
+// Hint: Each array element must appear in it's own html tag in the browser. Extra Credit: use map or reduce.
+// Extra Credit: use map or reduce
+
+const names = ['Bob', 'Jim', 'Matt', 'Mary', 'Suzy', 'Sarah']
+
+let target = document.getElementById('target')
+target.innerText = 'We changed the text.'
+console.log(target)
+
+names.forEach(name=>target.innerHTML = target.innerHTML + `<p>${name}</p>`)
+let p =document.createElement('p')
+p.innerText = 'this is a p tag'
+console.log(p)
