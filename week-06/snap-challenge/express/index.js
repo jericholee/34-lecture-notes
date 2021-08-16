e5296594b2881a3276e6bea56515cf8557408006
Jericho Ortiz <jortiz180@cnm.edu>
2021-08-16 10:10:42 -0600

    completed code along

--- week-06/snap-challenge/express/index.js
+++ week-06/snap-challenge/express/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const morgan = require('morgan')
+const {request, response} = require("express");
 
 const app = express()
 
@@ -7,5 +8,25 @@ app.use(morgan("dev"))
 app.use(express.json())
 
 // Add your code here.
+
+// Define our router from express object
+
+const indexRoute = express.Router()
+
+
+const middleware1 = function (request, response, nextFunction){
+	console.log('hello world')
+	nextFunction()
+}
+
+const middleware2 = (request, response, nextFunction) => {
+	return response.json({status:200, data: null, message: 'this thing is on'})
+}
+
+// set our router to respond to /apis path
+indexRoute.route('/apis').get(middleware1, middleware2)
+
+// register router to be used by express
+app.use(indexRoute)
 
 app.listen(4200, () => {console.log("Express has Started")})
